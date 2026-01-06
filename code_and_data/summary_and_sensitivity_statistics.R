36ec81cab3191bd05d2bfeaa5f17f996b4e16086
IgorImperial <i.pantea24@imperial.ac.uk>
2026-01-06 11:22:22 +0000

    Cleaning and comments

--- code_and_data/summary_and_sensitivity_statistics.R
+++ code_and_data/summary_and_sensitivity_statistics.R
@@ -1,5 +1,4 @@
-install.packages("plan")
-install.packages("ggh4x")
+# this script is for meta-analysis and sensitivity analysis within the context of the systematic review
 
 library(dplyr)
 library(ggplot2)
@@ -23,7 +22,6 @@ epidemiological <- filter(df, type_of_study_cleaned == "epidemiological") %>%
 epidemiological_for_pairwise <- unique(epidemiological) %>% filter(!is.na(coefficient) & coefficient_type == "pairwise correlation")
 epidemiological <- unique(epidemiological) %>% filter(!is.na(coefficient) & !(is.na(coefficient_lower_CI)))
 
-
 epidemiological <- mutate(epidemiological, coefficient = as.numeric(coefficient))
 epidemiological_for_pairwise <- mutate(epidemiological_for_pairwise, coefficient = as.numeric(coefficient))
 epidemiological_for_pairwise <- mutate(epidemiological_for_pairwise, coefficient_lower_CI = as.numeric(coefficient_lower_CI))
@@ -34,6 +32,7 @@ epidemiological <- mutate(epidemiological, coefficient_higher_CI = as.numeric(co
 epidemiological <- mutate(epidemiological, variance = (coefficient_higher_CI - coefficient_lower_CI)^2 / (1.96*2)^2)
 
 
+#  processing the meta-analysis for the studies presented in the main text by weighting with the inverse of the invariance 
 summary_presented <- epidemiological %>% filter(`CASP TOTAL` != "POOR" & statistical_significance != "insignificant") %>% group_by(environmental_variables_cleaned, coefficient_type, continuous_or_dichotomised) %>% summarise(number_presented = n()) %>% ungroup()
 pairwise_presented <- epidemiological_for_pairwise %>% filter(`CASP TOTAL` != "POOR" & statistical_significance != "insignificant") %>% group_by(environmental_variables_cleaned, coefficient_type, continuous_or_dichotomised) %>% summarise(number_presented = n()) %>% ungroup()
 
@@ -54,8 +53,7 @@ summary_presented <- rbind(summary_presented, pairwise_presented)
 summary_presented <- filter(summary_presented, !is.na(mean_presented))
 
 
-
-
+#  processing the meta-analysis for the studies included in the appendix by weighting with the inverse of the invariance 
 summary_presented_with_poor <- epidemiological %>% filter(statistical_significance != "insignificant") %>% group_by(environmental_variables_cleaned, coefficient_type, continuous_or_dichotomised) %>% summarise(number_presented_with_poor = n()) %>% ungroup()
 pairwise_presented_with_poor <- epidemiological_for_pairwise %>% filter(statistical_significance != "insignificant") %>% group_by(environmental_variables_cleaned, coefficient_type, continuous_or_dichotomised) %>% summarise(number_presented_with_poor = n()) %>% ungroup()
 
@@ -76,8 +74,7 @@ summary_presented_with_poor <- rbind(summary_presented_with_poor, pairwise_prese
 summary_presented_with_poor <- filter(summary_presented_with_poor, !is.na(mean_presented_with_poor))
 
 
-
-
+#  processing the meta-analysis for the studies included in the appendix by weighting with the inverse of the invariance 
 summary_best <- epidemiological %>% filter(`CASP TOTAL` != "POOR" & statistical_significance == "significant") %>% group_by(environmental_variables_cleaned, coefficient_type, continuous_or_dichotomised) %>% summarise(number_best = n()) %>% ungroup()
 pairwise_best <- epidemiological_for_pairwise %>% filter(`CASP TOTAL` != "POOR" & statistical_significance == "significant") %>% group_by(environmental_variables_cleaned, coefficient_type, continuous_or_dichotomised) %>% summarise(number_best = n()) %>% ungroup()
 
@@ -98,14 +95,10 @@ summary_best <- rbind(summary_best, pairwise_best)
 summary_best <- filter(summary_best, !is.na(mean_best))
 
 
-
-
+# keep relevant columns
 summary_presented <- summary_presented[, !(names(summary_presented) %in% c("first_author", "year_of_article", "environmental_variables", "CASP TOTAL", "statistical_significance", "coefficient", "coefficient_lower_CI", "coefficient_higher_CI", "variance"))]
-
 summary_presented_with_poor <- summary_presented_with_poor[, !(names(summary_presented_with_poor) %in% c("first_author", "year_of_article", "environmental_variables", "CASP TOTAL", "statistical_significance", "coefficient", "coefficient_lower_CI", "coefficient_higher_CI", "variance"))]
-
 summary_best <- summary_best[, !(names(summary_best) %in% c("first_author", "year_of_article", "environmental_variables", "CASP TOTAL", "statistical_significance", "coefficient", "coefficient_lower_CI", "coefficient_higher_CI", "variance"))]
-
 
 
 summary_presented <- summary_presented[!duplicated(summary_presented), ]
@@ -113,34 +106,19 @@ summary_presented_with_poor <- summary_presented_with_poor[!duplicated(summary_p
 summary_best <- summary_best[!duplicated(summary_best), ]
 
 
-
-# summary_all <- epidemiological %>% group_by(environmental_variables_cleaned, coefficient_type, continuous_or_dichotomised) %>% summarize(mean_all = mean(coefficient), min_all = min(coefficient), max_all = max(coefficient))
-
-# Ignore function: an attempt at collating everything in one function
-# summary_presented <- epidemiological %>% filter(`CASP TOTAL` != "POOR" & statistical_significance != "insignificant") %>% group_by(environmental_variables_cleaned, coefficient_type, continuous_or_dichotomised) %>% summarize(presented = paste(round(mean(coefficient[coefficient_type != "regression slope"]), 2), round(min(coefficient[coefficient_type != "regression slope"]), 2), round(max(coefficient[coefficient_type != "regression slope"]), 2), sep = ", "),                                                                                                                                                                                                                              presented2 = paste(round(mean(coefficient[coefficient_type == "regression slope"]), 4), round(min(coefficient[coefficient_type == "regression slope"]), 4), round(max(coefficient[coefficient_type == "regression slope"]), 4), sep = ", "))
-
-
+# join all summaries together
 summary <- full_join(summary_presented, summary_presented_with_poor, by = c("environmental_variables_cleaned", "coefficient_type", "continuous_or_dichotomised"))
-
 summary <- full_join(summary, summary_best, by = c("environmental_variables_cleaned", "coefficient_type", "continuous_or_dichotomised"))
-
-# summary <- full_join(summary, summary_all, by = c("environmental_variables_cleaned", "coefficient_type", "continuous_or_dichotomised"))
-
 remove(summary_presented, summary_presented_with_poor, summary_best)
-# remove(summary_all)
+
 
 summary <- filter(summary, !is.na(coefficient_type)) %>% group_by(environmental_variables_cleaned) %>% arrange(environmental_variables_cleaned, coefficient_type, continuous_or_dichotomised)
 
+# round to the appropriate number of digits / note: regression slope coefficient is small hence 4 digits.
 new <- summary %>% filter(coefficient_type == "regression slope") %>% mutate(across(is.numeric, round, digits = 4))
-
 summary <- summary %>% filter(coefficient_type != "regression slope") %>% mutate(across(is.numeric, round, digits = 2))
-
-# summary <- data.frame(lapply(summary, function(x) if(is.numeric(x)) round(x, 2) else x))
-
 summary <- rbind(summary, new)
 remove(new)
-
-
 
 
 # create summary statistics workbook
@@ -151,16 +129,8 @@ writeData(wb, "sensitivity_analysis", summary, startRow = 1, startCol = 1)
 
 saveWorkbook(wb, file = "figures_data/summary_and_sensitivity_analysis.xlsx", overwrite = TRUE)
 
-
-
-
-
-
-
-
-
-
-
+# Important note: the separation by type of meta-analysis (presented in the main text, and the included sensitivity analysis for poorer and better inclusion criteria for the meta-analysis) was done manually in excel
+# The changing from long to wide format as shown in the meta-analysis table in the main-text and supplementary materials was also done manually using pivot table and manual sorting
 
 
 
