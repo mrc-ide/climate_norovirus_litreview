be88e35ae0c928a48ef5e03e58bd7d233ba0897c
IgorImperial <i.pantea24@imperial.ac.uk>
2026-04-27 15:35:01 +0100

    corrected sheet name when reading excel file

--- code_and_data/summary_statistics.R
+++ code_and_data/summary_statistics.R
@@ -5,7 +5,7 @@ library(ggplot2)
 library(openxlsx)
 
 #read data
-df <- readxl::read_xlsx("Supplementary Materials 3.xlsx", sheet = "Data Detailed")
+df <- readxl::read_xlsx("Supplementary Materials 3.xlsx", sheet = "Table S9")
 
 df <- filter(df, environmental_variables != "pH")
 
@@ -35,8 +35,8 @@ summary <- df %>% select(covidence_id, type_of_study_cleaned, environmental_vari
 writeData(wb, "study_envrnm_direction_type", summary, startRow = 1, startCol = 1)
 
 #########################################################################
-# create a new sheet for by study, by environmental variable, direction of relationship for high quality and significant relationships
-addWorksheet(wb, "study_envrnm_direction_select")
+# create a new  for by study, by environmental variable, direction of relationship for high quality and significant relationships
+addWork(wb, "study_envrnm_direction_select")
 
 summary <- df %>% filter((`CASP TOTAL` == "GOOD" | `CASP TOTAL` == "ADEQUATE") & statistical_significance != "insignificant") %>%
                   select(covidence_id, type_of_study_cleaned, environmental_variables_cleaned, type_of_relationship_2, direction_of_relationship) %>%
@@ -54,8 +54,8 @@ summary$proportion <- round(summary$count/summary$count_all*100, 2)
 writeData(wb, "study_envrnm_direction_select", summary, startRow = 1, startCol = 1)
 
 ################################################################
-# sheet by study type and tested mechanisms of influence
-addWorksheet(wb, "study_mechanism_tested")
+#  by study type and tested mechanisms of influence
+addWork(wb, "study_mechanism_tested")
 
 summary <- df %>% select(covidence_id, type_of_study_cleaned, mechanism_of_influence_cleaned, mechanism_of_influence_tested_or_assumed, environmental_variables_cleaned) %>%
                   filter(mechanism_of_influence_tested_or_assumed == "tested") %>%
@@ -66,8 +66,8 @@ summary <- df %>% select(covidence_id, type_of_study_cleaned, mechanism_of_influ
 writeData(wb, "study_mechanism_tested", summary, startRow = 1, startCol = 1)
 
 ################################################################
-# sheet by study type and assumed mechanisms of influence
-addWorksheet(wb, "study_mechanism_assumed")
+#  by study type and assumed mechanisms of influence
+addWork(wb, "study_mechanism_assumed")
 
 summary <- df %>% select(covidence_id, type_of_study_cleaned, mechanism_of_influence_cleaned, mechanism_of_influence_tested_or_assumed, environmental_variables_cleaned) %>%
   filter(mechanism_of_influence_tested_or_assumed == "assumed") %>%
@@ -80,8 +80,8 @@ writeData(wb, "study_mechanism_assumed", summary, startRow = 1, startCol = 1)
 saveWorkbook(wb, file = "figures_data/summary_statistics.xlsx", overwrite = TRUE)
 
 ################################################################
-# sheet by study type and all mechanisms of influence
-addWorksheet(wb, "study_mechanism_all")
+#  by study type and all mechanisms of influence
+addWork(wb, "study_mechanism_all")
 
 summary <- df %>% select(covidence_id, type_of_study_cleaned, mechanism_of_influence_cleaned, environmental_variables_cleaned) %>%
   unique() %>%
